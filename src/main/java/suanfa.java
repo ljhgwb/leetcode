7e90eee5e93d493ef26e7d3258a31d5de20a10b9
ljhgwb <ljhgwb@github.com>
2026-08-15 18:41:20 +0800

    两数之和

--- src/main/java/suanfa.java
+++ src/main/java/suanfa.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 //判断n以内有多少个素数
 public class suanfa {
     public static void main(String[] args){
@@ -22,5 +25,30 @@ public class suanfa {
         return true;
     }
 }
+//两数之和  给定数组nums和整数目标值target,找出数组中和为目标值那俩整数，并返回他们下标
+class suanfa1{
+    public int[] twosum(int[] nums,int target) {
+        //创建哈希表
+        Map<Integer, Integer> storeNums = new HashMap<>(nums.length, 1 );
+        //接收返回下标
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            //找满足条件的数
+            int another = target - nums[i];
+            //去哈希表判断这个数存不存在，如果存在返回那个数下标
+            Integer anotherIndex = storeNums.get(another);
+            if(null!=anotherIndex){
+                result[0]=anotherIndex;
+                result[1]=i;
+                break;
+            }else{
+                //不存在就放入哈希表中
+                storeNums.put(nums[i],i);
+            }
+        }
+        return result;
+    }
 
+}
+//核心判断哈希表中是否存在需要的那个数
 
